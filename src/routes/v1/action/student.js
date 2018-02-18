43b3b113d594439fd57f6e3d400c5568700fdfa4
Milad Nekooei <mi.nekooei@gmail.com>
2018-02-18 14:41:33 +0330

    create checkRegister API endPoint

--- src/routes/v1/action/student.js
+++ src/routes/v1/action/student.js
@@ -1,16 +1,33 @@
 /**
  * Created by milad on 1/22/18.
  */
-import { Router } from 'express';
+import {Router} from 'express';
+import {check, validationResult} from 'express-validator/check';
+import {matchedData, sanitize} from 'express-validator/filter';
+import StudentProvider from '../provider/student';
+import ResponseGenerator from '../../../util/responseGenerator';
 
 export default ({config, database}) => {
   let api = new Router();
+  const studentProviderInstance = new StudentProvider(database);
 
   api.get('/', (req, res) => {
     res.send({
       message: 'greeting'
     });
   });
+
+  api.post('/checkRegister', async (req, res) => {
+    const registerInfo = await studentProviderInstance.checkRegistration(req.body.nationalCode);
+    if (registerInfo.length !== 0) {
+      res.send(ResponseGenerator(true, "You Can Login", registerInfo));
+    } else {
+      res.send(ResponseGenerator(false, "You Must Register First"));
+    }
+
+  });
+
+
 
   return api;
 }
