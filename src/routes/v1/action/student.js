56066b7f163d88c7fba9f965dc3ab023f8c4ec81
Milad Nekooei <mi.nekooei@gmail.com>
2018-02-18 17:01:39 +0330

    use body validation in checkRegister API endpoint

--- src/routes/v1/action/student.js
+++ src/routes/v1/action/student.js
@@ -1,11 +1,11 @@
 /**
  * Created by milad on 1/22/18.
  */
-import {Router} from 'express';
-import {check, validationResult} from 'express-validator/check';
-import {matchedData, sanitize} from 'express-validator/filter';
-import StudentProvider from '../provider/student';
-import ResponseGenerator from '../../../util/responseGenerator';
+import {Router} from "express";
+import {validationResult} from "express-validator/check";
+import * as BodyParser from "../../../middleware/bodyParser";
+import StudentProvider from "../provider/student";
+import ResponseGenerator from "../../../util/responseGenerator";
 
 export default ({config, database}) => {
   let api = new Router();
@@ -17,7 +17,11 @@ export default ({config, database}) => {
     });
   });
 
-  api.post('/checkRegister', async (req, res) => {
+  api.post('/checkRegister', BodyParser.checkRegisterSchema(), async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.send(ResponseGenerator(false, "Validation Error", errors.mapped()));
+    }
     const registerInfo = await studentProviderInstance.checkRegistration(req.body.nationalCode);
     if (registerInfo.length !== 0) {
       res.send(ResponseGenerator(true, "You Can Login", registerInfo));
