6fffd1316c64b96560faf94e137b7f530ea42cac
Milad Nekooei <mi.nekooei@gmail.com>
2018-02-18 18:32:57 +0330

    Create BodyParser Schema for register student

--- src/middleware/bodyParser.js
+++ src/middleware/bodyParser.js
@@ -9,3 +9,23 @@ export const checkRegisterSchema = () =>  [
     .isLength({min: 10, max:10}).withMessage('National Code must only 10 digits')
 ];
 
+export const registerSchema = () => [
+  check('email', 'required').isEmail().withMessage('Email is invalid').trim().normalizeEmail(),
+  check('password', 'Must to set password')
+    .isLength({min: 5})
+    .withMessage('Password must have more than 5 characters')
+    .matches(/\d/)
+    .withMessage('Password must have minimum one digit'),
+  check('homeLocation').trim().matches(/\(\d+\.\d+,\d+\.\d+\)/).withMessage('Invalid Point'),
+  check('nationalCode', 'National Code cannot be empty').isLength({
+    min: 10,
+    max: 10
+  }).withMessage('Invalid National Code'),
+  check('fullName','FullName cannot be empty').trim(),
+  check('address','Address cannot be empty').trim(),
+  check('gender','gender must selected').trim(),
+  check('description').trim().isLength({min: 0}),
+  check('birthDate', 'Birth Date required').trim().isLength({min: 8, max: 8}).withMessage('invalid birthDate format'),
+  check('postCode').trim().isLength({min: 10, max: 10}),
+  check('birthPlace').trim()
+];
