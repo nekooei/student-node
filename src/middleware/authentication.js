a4ce616e1eac538a9a82718f2c0b86fd64bb8c50
Milad Nekooei <mi.nekooei@gmail.com>
2018-03-03 13:38:38 +0330

    Customize messages for authentication

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -3,6 +3,7 @@
  */
 import jwt from 'jsonwebtoken';
 import expressjwt from 'express-jwt';
+import passport from 'passport';
 
 const TOKEN_TIME = 7 * 24 * 60 * 60;
 const SECRET = 'sagepireCSEHAM3MIDONanKi3!!!!!!!';
@@ -10,6 +11,7 @@ const SECRET = 'sagepireCSEHAM3MIDONanKi3!!!!!!!';
 let authenticate = expressjwt({secret: SECRET});
 
 let generateAccessToken = (req, res, next) => {
+  console.log(req.user);
   req.token = req.token || {};
   req.token = jwt.sign({
     id: req.user.id,
@@ -23,13 +25,28 @@ let generateAccessToken = (req, res, next) => {
 let respond = (req, res) => {
   res.status(200)
     .json({
-      user: req.user.username,
+      id: req.user.id,
+      nationalCode: req.user.nationalCode,
       token : req.token
     });
+};
+
+let customAuthenticateMessage = (req, res, next) =>{
+  return passport.authenticate('local', {
+    session: false,
+    scope: []
+  }, (error, thisModel, authError) => {
+    if(authError){
+      return res.status(401).send(authError);
+    }
+    req.user= thisModel;
+    next();
+  }) (req, res, next);
 };
 
 export default  {
   authenticate,
   generateAccessToken,
-  respond
+  respond,
+  customAuthenticateMessage
 }
