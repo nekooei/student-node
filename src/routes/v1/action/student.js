d243bb5334aa1a59a4c7b988d5118a89ac4c60a7
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-20 18:00:26 +0430

    request payment token

--- src/routes/v1/action/student.js
+++ src/routes/v1/action/student.js
@@ -8,6 +8,7 @@ import StudentProvider from "../provider/student";
 import ResponseGenerator from "../../../util/responseGenerator";
 import authMiddleWare from '../../../middleware/authentication';
 import DistanceMatrix from 'google-distance-matrix';
+import GatewayProvider from "../provider/gateway";
 
 const DISTANCE_MATRIX_API_KEY = 'AIzaSyAJRlHVa45YqX-0chPm5Y7JDiaplMIQMOM';
 
@@ -35,7 +36,7 @@ export default ({config, database}) => {
 
   });
 
-  api.get('/info', authMiddleWare.authenticate, async(req, res) => {
+  api.get('/info', authMiddleWare.authenticate, async (req, res) => {
     const studentInfo = await studentProviderInstance.getStudentInfo(req.user.id);
     res.send(ResponseGenerator(true, 'Student Info', studentInfo));
   });
@@ -45,8 +46,21 @@ export default ({config, database}) => {
     if (!errors.isEmpty()) {
       return res.send(ResponseGenerator(false, "Validation Error", errors.mapped()));
     }
-    const registerInfo = await studentProviderInstance.register(req.body);
-    res.send(registerInfo);
+    try {
+      const registerInfo = await studentProviderInstance.register(req.body);
+      res.send(ResponseGenerator(true, 'register successful', registerInfo.reduce
+        (({}, currentValue) => {
+          return {
+              studentId: currentValue.studentId,
+              fullName: currentValue.fullName,
+              nationalCode: currentValue.nationalCode
+          }
+        }, {})
+      ));
+    } catch (err) {
+      res.send(ResponseGenerator(false, 'operation failed', err))
+    }
+
   });
 
   api.post('/login', BodyParser.loginSchema()
@@ -65,9 +79,9 @@ export default ({config, database}) => {
 
   api.get('/school/:schoolId/openTerm', authMiddleWare.authenticate, async (req, res) => {
     const openTerm = await studentProviderInstance.getOpenTermOfSchool(req.params.schoolId);
-    if(openTerm.length !== 0 ){
+    if (openTerm.length !== 0) {
       res.send(ResponseGenerator(true, 'Open Term', openTerm[0]));
-    }else {
+    } else {
       res.send(ResponseGenerator(false, 'No Open Term Available'));
     }
 
@@ -78,62 +92,87 @@ export default ({config, database}) => {
     res.send(ResponseGenerator(true, 'Term Groups', termGroups));
   });
 
- api.get('/:schoolId/distance', authMiddleWare.authenticate, async (req, res) => {
-   try{
-     const userInfo = await studentProviderInstance.getStudentInfo(req.user.id);
-     const schoolInfo = await  studentProviderInstance.getSchool(req.params.schoolId);
-     if(schoolInfo.length === 0 ){
-       res.send(ResponseGenerator(false, "School Invalid"));
-       return;
-     }
-     const origin = [`${userInfo.homeLocation.x},${userInfo.homeLocation.y}`];
-     const destination = [`${schoolInfo[0].geoLocation.x},${schoolInfo[0].geoLocation.y}`];
-     DistanceMatrix.key(DISTANCE_MATRIX_API_KEY);
-     DistanceMatrix.matrix(origin, destination, (err, distance) => {
-      if(err){
-        res.send(ResponseGenerator(false, err.message, err));
+  api.get('/:schoolId/distance', authMiddleWare.authenticate, async (req, res) => {
+    try {
+      const userInfo = await studentProviderInstance.getStudentInfo(req.user.id);
+      const schoolInfo = await  studentProviderInstance.getSchool(req.params.schoolId);
+      if (schoolInfo.length === 0) {
+        res.send(ResponseGenerator(false, "School Invalid"));
         return;
       }
-      if(distance.status === 'OK'){
-        if(distance.rows[0].elements[0].status === 'ZERO_RESULTS'){
-          res.send(ResponseGenerator(false, 'No Route to destination!'));
+      const origin = [`${userInfo.homeLocation.x},${userInfo.homeLocation.y}`];
+      const destination = [`${schoolInfo[0].geoLocation.x},${schoolInfo[0].geoLocation.y}`];
+      DistanceMatrix.key(DISTANCE_MATRIX_API_KEY);
+      DistanceMatrix.matrix(origin, destination, (err, distance) => {
+        if (err) {
+          res.send(ResponseGenerator(false, err.message, err));
           return;
         }
-        const responseData = {
-          distanceMeter : distance.rows[0].elements[0].distance.value,
-          distanceText : distance.rows[0].elements[0].distance.text,
-        };
-        res.send(ResponseGenerator(true, "distances", responseData));
-      }else {
-        res.send(ResponseGenerator(false, 'error in distance'));
-      }
-     });
-   }catch (error){
-     res.send(ResponseGenerator(false, error.message, error));
-   }
- });
+        if (distance.status === 'OK') {
+          if (distance.rows[0].elements[0].status === 'ZERO_RESULTS') {
+            res.send(ResponseGenerator(false, 'No Route to destination!'));
+            return;
+          }
+          const responseData = {
+            distanceMeter: distance.rows[0].elements[0].distance.value,
+            distanceText: distance.rows[0].elements[0].distance.text,
+          };
+          res.send(ResponseGenerator(true, "distances", responseData));
+        } else {
+          res.send(ResponseGenerator(false, 'error in distance'));
+        }
+      });
+    } catch (error) {
+      res.send(ResponseGenerator(false, error.message, error));
+    }
+  });
 
- api.post('/servicePrice', authMiddleWare.authenticate, async (req, res) => {
-   const priceUnit = await studentProviderInstance.getTermPriceUnit(req.body.termId);
-   const termGroup = await studentProviderInstance.getTermGroup(req.body.termGroupId);
-   const schoolInformation = await studentProviderInstance.getSchool(req.body.schoolId);
-   const response = {
-     school: schoolInformation[0].caption,
-     priceUnit : priceUnit.price,
-     startDate : termGroup.startDate,
-     groupCaption: termGroup.caption,
-     endDate: termGroup.endDate,
-     distance: req.body.distance,
-     sessionCount: termGroup.sessionsCount,
-     totalPrice : ((termGroup.sessionsCount * 2) * priceUnit.price) * req.body.distance
-   };
-   res.send(ResponseGenerator(true, 'Service Price Review', response))
- });
+  api.post('/servicePrice', authMiddleWare.authenticate, async (req, res) => {
+    const priceUnit = await studentProviderInstance.getTermPriceUnit(req.body.termId);
+    const termGroup = await studentProviderInstance.getTermGroup(req.body.termGroupId);
+    const schoolInformation = await studentProviderInstance.getSchool(req.body.schoolId);
+    const response = {
+      school: schoolInformation[0].caption,
+      priceUnit: priceUnit.price,
+      startDate: termGroup.startDate,
+      groupCaption: termGroup.caption,
+      endDate: termGroup.endDate,
+      distance: req.body.distance,
+      sessionCount: termGroup.sessionsCount,
+      totalPrice: ((termGroup.sessionsCount * 2) * priceUnit.price) * req.body.distance
+    };
+    res.send(ResponseGenerator(true, 'Service Price Review', response))
+  });
 
   api.post('/createRequest', authMiddleWare.authenticate, async (req, res) => {
     req.body.studentId = req.user.id;
     const result = await studentProviderInstance.createServiceRequest(req.body);
     res.send(ResponseGenerator(true, 'Create Service', result[0]));
+  });
+
+  api.post('/requestPaymentToken', authMiddleWare.authenticate, async (req, res) => {
+    try {
+      const gatewayProviderInstance = new GatewayProvider(database);
+      const tokenRes = await gatewayProviderInstance.requestGatewayToken(req.body.amount * 10, `${String(Date.now())}-${req.body.amount}-${req.user.id}`);
+      res.send(ResponseGenerator(true, 'Token', tokenRes[0]));
+    } catch (error) {
+      res.send(error);
+    }
+
+  });
+
+  api.post('/verify', (req, res) => {
+    console.log('params ', req.params);
+    console.log('body ', req.body);
+    console.log('query ', req.query);
+    res.redirect('http://google.com');
+  });
+
+  api.get('/verify', (req, res) => {
+    console.log('params ', req.params);
+    console.log('body ', req.body);
+    console.log('query ', req.query);
+    res.send({'hello': ''})
   });
 
   return api;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,12 +3,15 @@
  */
 import Express from 'express';
 import StudentActionsCreator from './action/student';
+import PaymentActionCreator from './action/payment';
 
 
 export default ({config, database}) => {
   let router = new Express();
 
   router.use('/student', StudentActionsCreator({config, database}));
+
+  router.use('/payment', PaymentActionCreator({ config, database}));
 
   return router;
 }
