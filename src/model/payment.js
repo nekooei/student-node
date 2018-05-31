369117269601c656de2ea0105c4af382f0b03335
Milad Nekooei <mi.nekooei@gmail.com>
2018-05-31 16:18:20 +0430

    Create PaymentModel with all required functions

---
+++ src/model/payment.js
@@ -0,0 +1,39 @@
+import MyModel from './myModel';
+
+
+class PaymentModel extends MyModel{
+  constructor(database){
+    super({
+      name: 'payment',
+      database
+    });
+  }
+
+  createNewPayment(gatewayId, amount, transId, serviceRequestId, studentId){
+    return this.runFunction('student', this.name, 'create', {
+      args: [studentId, serviceRequestId, gatewayId, transId,  amount]
+    });
+  }
+
+  setDetail(transId, detail){
+    return this.runFunction('student', this.name, 'updateDetail', {
+      args : [transId, detail]
+    })
+  }
+
+  getByTransId(transId){
+    return this.runFunction('student', this.name, 'findByTransId', {
+      args: [transId]
+    })
+  }
+
+  verifyPayment(transId, verify=true){
+    return this.runFunction('student', this.name, 'verify', {
+      args: [transId, verify]
+    })
+  }
+
+
+
+
+}
