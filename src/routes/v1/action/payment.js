94ddc41ff2745fbb3489128e15211887b1238fa6
Milad Nekooei <mi.nekooei@gmail.com>
2018-06-09 17:13:27 +0430

    Create Payment API

--- src/routes/v1/action/payment.js
+++ src/routes/v1/action/payment.js
@@ -1,8 +1,40 @@
 import {Router} from "express";
+import authMiddleWare from '../../../middleware/authentication';
+import PayIR from '../../../util/payir'
+import PaymentProvider from '../provider/payment';
+import GatewayProvider from "../provider/gateway";
+import ResponseGenerator from "../../../util/responseGenerator";
+
 
 export default ({config, database}) => {
   let api = new Router();
+  const paymentProviderInstance = new PaymentProvider(database);
+  const gatewayProviderInstance = new GatewayProvider(database);
 
+  api.post('/init', authMiddleWare.authenticate ,async (req, res) => {
+    try{
+      const gatewayDetail = await gatewayProviderInstance.getById(req.body.gatewayId);
+      if(gatewayDetail.length === 0){
+        throw new URIError('No Gateway Found');
+      }else {
+        const gatewayHandler = new PayIR(gatewayDetail[0].apikey);
+        const factorNum = `${String(Date.now())}-${req.body.amount}-${req.user.id}`;
+        const transId = await gatewayHandler.send(req.body.amount, 'http://localhost:65000/v1/payment/verify', factorNum);
+        const paymentRecord = await paymentProviderInstance.createNewPayment({...req.body, transId: String(transId), studentId: req.user.id});
+        res.json(ResponseGenerator(true, 'payment', paymentRecord));
+
+
+      }
+    }catch (e) {
+      res.json(ResponseGenerator(false, e.message, e));
+    }
+  });
+
+  api.post('/verify', async(req, res) => {
+    console.log(req.body);
+    res.json({...req.body});
+    res.redirect('http://student.development.sas/panel/success')
+  });
 
   return api;
 
