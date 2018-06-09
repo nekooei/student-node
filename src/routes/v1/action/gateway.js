f6c37d38a2a2921e2bd21d4a1c635b96222a9c1b
Milad Nekooei <mi.nekooei@gmail.com>
2018-06-09 14:32:16 +0430

    Hide APIKEY from listing result

--- src/routes/v1/action/gateway.js
+++ src/routes/v1/action/gateway.js
@@ -1,21 +1,26 @@
 import {Router} from "express";
-import authMiddleWare from '../../../middleware/authentication';
 import GatewayProvider from '../provider/gateway';
 import ResponseGenerator from "../../../util/responseGenerator";
+import authMiddleWare from '../../../middleware/authentication';
 
 
 export default ({config, database}) => {
   let api = new Router();
   const gatewayProviderInstance = new GatewayProvider(database);
 
-  //todo : add authentication middleware after all test passed
-    api.get('/', async (req, res) => {
-      try{
-        const gateways = await gatewayProviderInstance.getAllGateways();
-        res.json(ResponseGenerator(true, 'gateways', gateways))
-      }catch (e) {
-        res.json(ResponseGenerator(false, e.message, e))
-      }
+  api.get('/', authMiddleWare, async (req, res) => {
+    try {
+      const gateways = await gatewayProviderInstance.getAllGateways();
+      res.json(ResponseGenerator(true, 'gateways', gateways.map(gateway => {
+          return {
+            ...gateway,
+            apikey: undefined
+          }
+        })
+      ));
+    } catch (e) {
+      res.json(ResponseGenerator(false, e.message, e))
+    }
 
   });
 
